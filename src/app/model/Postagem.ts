470ef622e1e40fa01713e629c9db01d47780f48f
Kauana Andrade <kauana.andrade151116@gmail.com>
2021-11-18 09:28:44 -0300

    Rotas de entrar e cadastrar

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,11 @@
+import { Tema } from "./Tema"
+import { User } from "./User"
+
+export class Postagem{
+    public idPostagem: number
+    public titulo: string
+    public descricao: string
+    public dataPostagem: Date
+    public usuario: User
+    public tema: Tema
+}
